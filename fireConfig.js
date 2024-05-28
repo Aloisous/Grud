a045ec63e537200bd3c36d88ac60bbd0811c2ea5
Aloisous <163976503+Aloisous@users.noreply.github.com>
2024-05-29 00:02:20 +0800

    Create fireConfig.js

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBu_U7n-VhAJGqF6DWwOkDrlgZzpHQ5y4A",
+  authDomain: "project-6261142026610774088.firebaseapp.com",
+  databaseURL: "https://project-6261142026610774088-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "project-6261142026610774088",
+  storageBucket: "project-6261142026610774088.appspot.com",
+  messagingSenderId: "972358770604",
+  appId: "1:972358770604:web:bfa8abeda009d4bc89a409"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
